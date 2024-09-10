6ecd48f932d5193d43be510f561ee6591050f96f
Roman <workprogramm314@gmail.com>
2024-09-10 22:54:36 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int j = 0;
+            int sum = 1;
+            int count = 0;
+            Console.WriteLine("Введиет кол-во эл");
+            int elementmasiv = int.Parse(Console.ReadLine());
+
+            int[] Array = new int[elementmasiv];
+            Console.WriteLine("заполните массив:");
+            for (int i = 0; i < Array.Length; i++)
+            {
+                Console.Write("Элемент " + i + ":\t");
+                Array[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Write("Вывод массива:");
+            for (int i = 0; i < Array.Length; i++)
+            {
+                Console.Write("\t" + Array[i]);
+                j = 2;
+                sum = 1;
+
+                for (j = 2; j < Array[i] / 2 + 1; j++)
+                {
+                    if (Array[i] % j == 0 && Array[i] != j)
+                    {
+                        sum = sum + j;
+
+                    }
+                    if (sum == Array[i])
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine("\nкол-во совершеных чисел:\t" + count);
+
+            Console.ReadLine();
+        }
+    }
+}
